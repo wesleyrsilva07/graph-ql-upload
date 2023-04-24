56fab785fdadc0852d22eb350811e3083bf001eb
Wesley Ribeiro da Silva <wesley.ribeiro@compasso.com.br>
2023-04-24 17:41:03 -0300

    Adjustments

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,22 +5,8 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
-  // @Get()
-  // getHello(): string {
-  //   return this.appService.getHello();
-  // }
-
-  // @Get('email')
-  // @Render('form')
-  // exibirForm() {
-  //   //
-  // }
-
-  // @Post('enviar-email')
-  // enviarEmail(@Request() req) {
-  //   return this.appService.enviarEmail(
-  //     req.body.email,
-  //     req.body.mensagem,
-  //   );
-  // }
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,18 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  // getHello(): string {
-  //   return 'Hello World!';
-  // }
-
-  // async enviarEmail(email: string, mensagem: string) {
-  //   await this.mailerService.sendMail({
-  //     to: email,
-  //     from: 'wesley.ribeiro@estudante.ifgoiano.edu.br',
-  //     subject: 'Enviando Email com NestJS',
-  //     html: `<h3 style="color: red">${mensagem}</h3>`,
-  //   });
-  // }
-
-
+  getHello(): string {
+    return 'Hello World!';
+  }
 }
